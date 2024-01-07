eb11549fa97365c1457f137f6ed97e8ebcfcae83
Andy Jones <3579815+wandering-andy@users.noreply.github.com>
2024-01-07 20:03:03 +0000

    Added initial validate file with TODOs

---
+++ readit/commands/validate.py
@@ -0,0 +1,5 @@
+# Performs validation of URL
+# If URL is valid, returns True
+# If URL is invalid, returns False, with error message
+# If URL is invalid add tags ERROR, ERROR_TYPE to URL in database
+# If URL is not provided, returns False, with error message
